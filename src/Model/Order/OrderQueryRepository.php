55661f527f1646f42981e5fc062c4f7aeca147d4
val77 <val77xdd@gmail.com>
2020-09-18 15:24:13 +0200

    fix unwanted use of OrderView

--- src/Model/Order/OrderQueryRepository.php
+++ src/Model/Order/OrderQueryRepository.php
@@ -2,17 +2,15 @@
 
 namespace App\Model\Order;
 
-use App\Entity\Order;
-
 interface OrderQueryRepository
 {
-    public function get(int $id): Order;
+    public function get(int $id): OrderView;
 
     /**
      * @param int $page
      * @param int $perPage
      *
-     * @return Order[]
+     * @return OrderView[]
      */
     public function fetchAll(int $page, int $perPage): array;
 
